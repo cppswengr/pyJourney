afc4fda9818c4c3087a369745bc8aa4533e0956d
cppswengr <cppswengr@eVantageGroup.com>
2018-11-18 01:54:36 -0600

    Modified primes.else creating get_primes function; started on ch4, upto but not including 'Hide implementation details' on page 106

--- ch3/primes.else.py
+++ ch3/primes.else.py
@@ -1,12 +1,25 @@
 primes = []                     # this will contain the primes in the end
 upto = 100                      # the limit, inclusive
-r
-for n in range(2, upto + 1):
 
-    for divisor in range(2, n):
-        if n % divisor == 0:
-            break
-    else:
-        primes.append(n)
 
-print(primes)
+
+def get_prime_numbers(upto=10):
+    """
+    Return prime numbers upto the specified (and inclusive) limit
+    :param upto:
+    :return:
+    """
+
+    primes = []                 # this will contain the primes
+    for n in range(2, upto + 1):
+
+        for divisor in range(2, n):
+            if n % divisor == 0:
+                break
+        else:
+            primes.append(n)
+
+    return primes
+
+if __name__ == "__main__":
+    print(get_prime_numbers(upto=100))
